821e2cf0691fc0f3a7c244b3b3e623d81dabc166
SLFireFlash <sllahiruprasanna@gmail.com>
2023-02-26 12:24:11 +0530

    databse connction testing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-const txtToken = document.getElementById("TextToken");
+const MongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+const uri = process.env.DATABASE_URL;
+
 
 function tokenGen(){
     var numbers =[1,2,3,4,5,6,7,8,9,0];
@@ -8,4 +14,38 @@ function tokenGen(){
     }
     return TokenId
 }
-txtToken.value = tokenGen();
+const txtToken = tokenGen();
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to the MongoDB server.
+client.connect(err => {
+  if (err) {
+    console.log('Failed to connect to MongoDB:', err);
+    return;
+  }
+
+  console.log('Connected to MongoDB!');
+
+  // Select the database and collection you want to work with.
+  const db = client.db('<chat-txt>');
+  const collection = db.collection('<chat-txt-an>');
+
+  // Get the value of the textbox.
+  const textboxValue = txtToken;
+
+  // Create a new document object with the textbox value.
+  const document = { value: textboxValue };
+
+  // Insert the document into the collection.
+  collection.insertOne(document, (err, result) => {
+    if (err) {
+      console.log('Failed to insert document:', err);
+      return;
+    }
+
+    console.log('Document inserted successfully!');
+  });
+
+
+});
