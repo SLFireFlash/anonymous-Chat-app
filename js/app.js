46f9b181114a651b3d5cad7ef16783bd21d999f0
SLFireFlash <sllahiruprasanna@gmail.com>
2023-02-26 18:56:50 +0530

    front end done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
-const dotenv = require('dotenv');
-
-
-dotenv.config();
-const uri = process.env.DATABASE_URL;
+const txtToken = document.getElementById("TextToken");
 
 
 function tokenGen(){
@@ -14,38 +9,5 @@ function tokenGen(){
     }
     return TokenId
 }
-const txtToken = tokenGen();
+txtToken.value = tokenGen();
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Connect to the MongoDB server.
-client.connect(err => {
-  if (err) {
-    console.log('Failed to connect to MongoDB:', err);
-    return;
-  }
-
-  console.log('Connected to MongoDB!');
-
-  // Select the database and collection you want to work with.
-  const db = client.db('<chat-txt>');
-  const collection = db.collection('<chat-txt-an>');
-
-  // Get the value of the textbox.
-  const textboxValue = txtToken;
-
-  // Create a new document object with the textbox value.
-  const document = { value: textboxValue };
-
-  // Insert the document into the collection.
-  collection.insertOne(document, (err, result) => {
-    if (err) {
-      console.log('Failed to insert document:', err);
-      return;
-    }
-
-    console.log('Document inserted successfully!');
-  });
-
-
-});
